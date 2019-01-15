e3989d3a7644f6616ccd1fe3d4afbb9492122e55
massaman_curry <m.m.elcapitan@gmail.com>
2019-01-15 22:10:47 +0900

    admin/user#destroy function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -31,6 +31,12 @@ class Admin::UsersController < ApplicationController
   def index
   end
 
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to admin_users_url, flash: {:notice => "ユーザー#{@user.name}を削除しました。"}
+  end
+
   private
 
   def user_params
