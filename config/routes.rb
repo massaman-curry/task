fea9f22cf3df45aa283f8c599ae58b11021520cf
massaman_curry <m.m.elcapitan@gmail.com>
2019-01-16 19:01:12 +0900

    imprement a login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   namespace :admin do
     resources :users
   end
+
   resources :tasks
+
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
